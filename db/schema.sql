1bde8a4842cf3592e3c2cb79dce1bc751dfcdc30
Chris <chrisbracewell35@gmail.com>
2022-03-06 14:49:58 -0500

    adding updates to schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,9 +7,3 @@ CREATE DATABASE ecommerce_db;
 -- Use the newly created database -- 
 USE ecommerce_db 
 
--- Drop and create tables -- 
-DROP TABLE IF EXISTS category ;
-DROP TABLE IF EXISTS product;
-DROP TABLE IF EXISTS tag;
-DROP TABLE IF EXISTS productTag;
-
