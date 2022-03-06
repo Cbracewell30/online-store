058ca285cf1205043c163bab9ddf4da4517ea867
Chris <chrisbracewell35@gmail.com>
2022-03-06 14:49:10 -0500

    updating schema file

--- db/schema.sql
+++ db/schema.sql
@@ -13,28 +13,3 @@ DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS tag;
 DROP TABLE IF EXISTS productTag;
 
-
--- CREATE TABLE category (
---     id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
---     category_name VARCHAR(30) NOT NULL
--- );
-
--- CREATE TABLE product (
---     id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
---     product_name VARCHAR(30) NOT NULL,
---     price DECIMAL (10,2) NOT NULL,
---     stock INTEGER DEFAULT (10) CHECK (stock>=1),
---     category_id
---  INTEGER references category (id)
--- );
-
--- CREATE TABLE tag (
---      id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
---      tag_name VARCHAR(30) 
--- );
-
--- CREATE TABLE productTag (
---      id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
---      product_id INTEGER references product(id),
---      tag_id INTEGER references tag(id)
--- );
